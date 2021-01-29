15651dd45bbbea011b5e702d1825f2bd370842bd
ajay7231 <53226413+ajay7231@users.noreply.github.com>
2021-01-29 17:32:41 +0530

    new

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require("express");
+const mongoose = require("mongoose");
+
+const shortUrl = require("./models/shortUrl");
+
+const app = express();
+
+mongoose.connect("mongodb://localhost/urlShortner", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+});
+app.set("view engine", "ejs");
+app.use(express.urlencoded({ extended: false }));
+
+app.get("/", async (req, res) => {
+  const shortUrls = await shortUrl.find()
+  res.render("index",{shortUrls:shortUrls});
+});
+
+app.post("/shortUrls", async (req, res) => {
+  await shortUrl.create({
+    full: req.body.fullUrl,
+  });
+  res.redirect("/");
+});
+
+app.get('/:shortUrl',async (req,res)=>{
+    const shorturl = await shortUrl.findOne({short:req.params.shortUrl})
+    if(shorturl == null) return res.sendStatus(404)
+    
+    shorturl.clicks++;
+    shorturl.save()
+
+    res.redirect(shorturl.full)
+})
+
+app.listen(process.env.PORT || 5000);
+console.log("Listenng on port...");
